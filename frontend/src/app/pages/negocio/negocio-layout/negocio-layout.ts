2d5515aa0e227005fa650fa1ad8ba75b0f710b68
Andy Chavez <Andy389@gmail.com>
2026-07-02 04:40:12 -0500

    Actualizar negocio-layout

--- frontend/src/app/pages/negocio/negocio-layout/negocio-layout.ts
+++ frontend/src/app/pages/negocio/negocio-layout/negocio-layout.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-negocio-layout',
   standalone: true,
@@ -9,20 +10,29 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   styleUrl: './negocio-layout.css',
 })
 export class NegocioLayout implements OnInit {
+
   negocioActual: any = null;
 
   ngOnInit(): void {
+
     const negocio = localStorage.getItem('negocio');
+
     if (negocio) {
       this.negocioActual = JSON.parse(negocio);
     }
+
   }
 
   inicialNegocio(): string {
+
     if (!this.negocioActual?.nombreNegocio) {
       return 'SG';
     }
 
-    return this.negocioActual.nombreNegocio.charAt(0).toUpperCase();
+    return this.negocioActual.nombreNegocio
+      .charAt(0)
+      .toUpperCase();
+
   }
+
 }
